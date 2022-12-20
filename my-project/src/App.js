b21112ca2ec033d1fee276063e4a1fc5e590dcf4
bewe <16521486@mahasiswa.itb.ac.id>
2022-12-20 22:04:36 +0700

    web setup

---
+++ my-project/src/App.js
@@ -0,0 +1,32 @@
+export default function App() {
+  return (
+    <div className="bg-black h-[1000px]">
+      <div className="grid-cols-2 grid h-[210px]">
+        <div className="mt-[220px] mr-[100px]">
+          <div className="font-mono font-bold text-[30px] text-white">
+            Hello, it's me
+          </div>
+          <div className="font-mono font-extrabold text-[70px] text-white mt-[-20px]">
+            Bernardus
+          </div>
+          <div className="flex">
+            <div className="font-mono font-extrabold text-[70px] text-white ml-[50px] mt-[-30px]">
+              Willson
+            </div>
+            <div className="font-mono font-extrabold text-[100px] text-[#5f99ff] mt-[-63px]">
+              .
+            </div>
+          </div>
+          <div className="font-mono text-[18px] text-slate-400 mt-[-20px]">
+          An enthusiastic product designer currently shaping the future of software development by designing smooth user-interfaces that promote user interaction with information and data. While traveling around the world.
+          </div>
+        </div>
+        <div className="mx-[50px]">
+          <div className="font-mono text-white">
+            hai
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
